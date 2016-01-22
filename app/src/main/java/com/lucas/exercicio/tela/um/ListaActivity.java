81725ff151f06bef200c8600bfc8a4661b33a1ce
Lucas Campos <lucasgcampos.contato@gmail.com>
2016-01-21 23:37:09 -0200

    Reuse fragments - Optimize life cicle

--- app/src/main/java/com/lucas/exercicio/tela/um/ListaActivity.java
+++ app/src/main/java/com/lucas/exercicio/tela/um/ListaActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
@@ -11,30 +12,36 @@ import com.lucas.exercicio.R;
 
 public class ListaActivity extends AppCompatActivity {
 
+    public static final String LISTA_FRAGMENT = "home";
+    public static final String NO_CONNECTION_FRAGMENT = "no-connection";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista);
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
         carregarFragment();
     }
 
     public boolean carregarFragment() {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
 
         boolean conectado = isConectado();
 
         if (conectado) {
-            transaction.replace(R.id.tela, new LocalFragment());
+            boolean hasListaFragment = fragmentManager.popBackStackImmediate(LISTA_FRAGMENT, 0);
+            if (!hasListaFragment) {
+                transaction.replace(R.id.tela, new LocalFragment()).addToBackStack(LISTA_FRAGMENT);
+            }
         } else {
-            transaction.replace(R.id.tela, new SemConexaoFragment());
+            boolean hasNoConnectionFragment = fragmentManager.popBackStackImmediate(NO_CONNECTION_FRAGMENT, 0);
+            if (!hasNoConnectionFragment) {
+                transaction.replace(R.id.tela, new SemConexaoFragment()).addToBackStack(NO_CONNECTION_FRAGMENT);
+            }
         }
 
-        transaction.addToBackStack(null).commit();
+        transaction.commit();
 
         return conectado;
     }
